c17f2e9c0659fe4d06ae06f6019b992b396ae6e4
unknown <minhduc180104@gmail.com>
2025-05-30 19:19:56 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import itemRouter from './routes/itemRouter.js';
+import userRouter from './routes/userRouter.js';
+
+await mongoose.connect('mongodb+srv://minhduc180104:minhduc180104@learnmongo.zli6q.mongodb.net/store?retryWrites=true&w=majority&appName=LearnMongo')
+
+const corsOptions = {
+    origin: 'http://localhost:5173',  
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],  
+    credentials: true, 
+};
+
+const app = express();
+app.use(express.json());
+app.use(cors(corsOptions));
+
+app.use('/items', itemRouter)
+app.use('/users', userRouter)
+
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "hello!" });
+});
+
+app.listen(8080, () => {
+    console.log("Server is running")
+})
