ce98ea40b190d1765324fba662d8b59a8c34f051
unknown <minhduc180104@gmail.com>
2025-06-04 20:19:58 +0700

    .

--- controller/orderController.js
+++ controller/orderController.js
@@ -1,15 +1,36 @@
 import orderModel from "../model/orders.schema.js";
+import userModel from "../model/user.schema.js";
 
 const orderController = {
     postOrder: async (req, res) => {
-        const userID = req.user.userId;
-        const data = req.body;
-        const rs = await orderModel.create({
-            ...data,
-            status: 'pending',
-            userID: userID,
-        })
-        return res.status(200).send(rs)
+        try {
+            const userID = req.user.userId;
+            const data = req.body;
+
+            const newOrder = await orderModel.create({
+                ...data,
+                status: 'pending',
+                userID: userID,
+            });
+
+            const orderedItemIDs = data.orders.map(order => order.itemID);
+
+            await userModel.updateOne(
+                { _id: userID },
+                {
+                    $pull: {
+                        cart: {
+                            itemID: { $in: orderedItemIDs }
+                        }
+                    }
+                }
+            );
+
+            return res.status(200).send(newOrder);
+        } catch (error) {
+            console.error('Order failed:', error);
+            return res.status(500).send({ message: 'Order failed', error });
+        }
     },
 
     getOrderByUserID: async (req, res) => {
